ca990c343416ae93b502292d469d6e5107e94bae
Sean Hansen-Busby <seanmhb@outlook.com>
2024-12-12 22:59:21 +0000

    Modified - Account Service Logic

--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -34,16 +34,19 @@ public class AccountService {
     */
     public ResponseEntity<Account> registerAccount(Account account)
     {
-        //Getting status code to determine if account has valid parameters
-        HttpStatus checkStatus = accountRepository.validAccountParameters(account.getUsername(), account.getPassword());
+        //Checking if account is valid to register
+        if(account.getUsername().equals("") == true || account.getPassword().equals("") == true || account.getUsername().length() < 4)
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
 
-        //Checking status code and returning if bad status
-        if(checkStatus == HttpStatus.BAD_REQUEST || checkStatus == HttpStatus.CONFLICT)
-            return ResponseEntity.status(checkStatus).body(null);
+        //Checking if duplicate account exists
+        if(accountRepository.findByUsername(account.getUsername()).get() != null)
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(null);
+        
+        //Persisting account to database
+        accountRepository.save(account);
 
-        //Registering account and returning a response entity
-        return accountRepository.registerAccount(account);
-
+        //Getting account to return with id
+        return ResponseEntity.ok(accountRepository.findByUsername(account.getUsername()).get());
     }
 
 
@@ -57,8 +60,15 @@ public class AccountService {
     */
     public ResponseEntity<Account> logInAccount(Account account)
     {
-        //Attempting to log into account and returning results
-        return accountRepository.logInAccount(account.getUsername(), account.getPassword());
+        //Getting account to check
+        Account loggedAccount = accountRepository.findByUsernameAndPassword(account.getUsername(), account.getPassword()).get();
+
+        //Checking if account matches username and password
+        if(loggedAccount == null)
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
+
+        //Account exists so returning account
+        return ResponseEntity.ok(loggedAccount);    
     }
 
 }
