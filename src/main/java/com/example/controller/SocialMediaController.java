1b680f70cf47c0f623d57e4529444cf24b541164
Sean Hansen-Busby <seanmhb@outlook.com>
2024-12-12 21:21:53 +0000

    Added - Controller Implementation and Documentation

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -6,6 +6,7 @@ import com.example.entity.Message;
 import com.example.service.AccountService;
 import com.example.service.MessageService;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -22,13 +23,16 @@ import javax.websocket.server.PathParam;
  * where applicable as well as the @ResponseBody and @PathVariable annotations. You should
  * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
  */
-@RestController
+@Controller
 public class SocialMediaController {
 
     private final AccountService accountService;
     private final MessageService messageService;
 
 
+    /*
+     * Constructor
+     */
     @Autowired
     public SocialMediaController(AccountService accountService, MessageService messageService)
     {
@@ -37,62 +41,120 @@ public class SocialMediaController {
     }
 
 
+    /**
+     * Handler to post a new registration.
+     * 
+     * @param account converted json representation of an account to be registered
+     * 
+     * @return A response entity containing the registered account and status code
+     */
     @PostMapping(value = "/register")
     public @ResponseBody ResponseEntity<Account> postRegistrationHandler(@RequestBody Account account)
     {
-        return ResponseEntity.ok(account);
+        //Will return with status code and registered account
+        return accountService.registerAccount(account);
     }
 
 
+    /**
+     * Handler to post a login attempt.
+     * 
+     * @param account converted json representation of an account to be logged in
+     * 
+     * @return A response entity containing the logged in account and status code
+     */
     @PostMapping(value = "/login")
     public @ResponseBody ResponseEntity<Account> postLoginHandler(@RequestBody Account account)
     {
-        return ResponseEntity.ok(account);
+        //Will return with status code and logged in account
+        return accountService.logInAccount(account);
     }
 
 
+    /**
+     * Handler to post a new message.
+     * 
+     * @param message converted json representation of a message to be created
+     * 
+     * @return A response entity containing the created message and status code
+     */
     @PostMapping(value = "/messages")
     public @ResponseBody ResponseEntity<Message> postMessagesHandler(@RequestBody Message message)
     {
-        return ResponseEntity.ok(message);
+        //Will return with status code and created message
+        return messageService.createMessage(message);
     }
 
 
+    /**
+     * Handler to get all messages.
+     * 
+     * @return A response entity containing a list of messages and a status code
+     */
     @GetMapping("/messages")
     public @ResponseBody ResponseEntity<List<Message>> getAllMessagesHandler()
     {
-        List<Message> allMessages = new ArrayList();
-        return ResponseEntity.ok(allMessages);
+        //Will return with status code and all messages
+        return messageService.getAllMessages();
     }
 
 
+    /**
+     * Handler to get a message by id.
+     * 
+     * @param Id An integer id for a message
+     * 
+     * @return A response entity containing all messages and a status code
+     */
     @GetMapping("/messages/{message_id}")
     public @ResponseBody ResponseEntity<Message> getMessageByIdHandler(@PathVariable int Id)
     {
-        Message m = new Message();
-        return ResponseEntity.ok(m);
+        //Will return with status code and a message
+        return messageService.getMessageById(Id);
     }
 
 
+    /**
+     * Handler to delete a message by id.
+     * 
+     * @param Id An integer id for a message
+     * 
+     * @return A response entity containing a deleted message and a status code
+     */
     @DeleteMapping("/messages/{message_id}")
     public @ResponseBody ResponseEntity<Message> deleteMessageByIdHandler(@PathVariable int Id)
     {
-        Message m = new Message();
-        return ResponseEntity.ok(m);
+        //Will return with status code and a deleted message
+        return messageService.deleteMessageById(Id);
     }
 
 
+    /**
+     * Handler to patch a message by id.
+     * 
+     * @param Id An integer id for a message
+     * 
+     * @return A response entity containing an updated message and a status code
+     */
     @PatchMapping("/messages/{message_id}")
     public @ResponseBody ResponseEntity<Message> patchMessageByIdHandler(@PathVariable int Id)
     {
-        Message m = new Message();
-        return ResponseEntity.ok(m);
+        //Will return with status code and an updated message
+        return messageService.patchMessageById(Id);
     }
 
+
+    /**
+     * Handler to get all messages from an account.
+     * 
+     * @param Id An integer id for an account
+     * 
+     * @return A response entity containing a list of account messages and a status code
+     */
     @GetMapping("/accounts/{account_id}/messages")
     public @ResponseBody ResponseEntity<List<Message>> getAccountMessagesHandler(@PathVariable int Id)
     {
-        List<Message> allMessages = new ArrayList();
-        return ResponseEntity.ok(allMessages);
+        //Will return with status code and a message list
+        return messageService.getMessagesByAccountId(Id);
     }
 }
